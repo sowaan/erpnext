cdbf56cfabc5b4f2346dffaa7df82287c7cb40d3
sowaan <137157787+sowaan@users.noreply.github.com>
2025-05-27 10:27:59 +0500

    feat:SowaanERP Branding

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -7,7 +7,7 @@ app_color = "#e74c3c"
 app_email = "hello@frappe.io"
 app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
-app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
+app_logo_url = "/assets/erpnext/images/favicon.png"
 
 
 add_to_apps_screen = [
@@ -110,8 +110,8 @@ calendars = ["Task", "Work Order", "Sales Order", "Holiday List", "ToDo"]
 website_generators = ["BOM", "Sales Partner"]
 
 website_context = {
-	"favicon": "/assets/erpnext/images/erpnext-favicon.svg",
-	"splash_image": "/assets/erpnext/images/erpnext-logo.svg",
+	"favicon": "/assets/erpnext/images/favicon.png",
+	"splash_image": "/assets/erpnext/images/favicon.png",
 }
 
 # nosemgrep
@@ -479,13 +479,13 @@ scheduler_events = {
 	],
 }
 
-email_brand_image = "assets/erpnext/images/erpnext-logo.jpg"
+email_brand_image = "assets/erpnext/images/favicon.png"
 
 default_mail_footer = """
 	<span>
 		Sent via
-		<a class="text-muted" href="https://frappe.io/erpnext?source=via_email_footer" target="_blank">
-			ERPNext
+		<a class="text-muted" href="https://www.sowaanerp.com?source=via_email_footer" target="_blank">
+		  SowaanERP
 		</a>
 	</span>
 """
